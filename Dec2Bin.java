4252976df2d4c15a0746f060a8bb7a24196b7edc
Soham Gupta <97831613+gupta-soham@users.noreply.github.com>
2022-10-07 12:29:24 +0530

    Created a file to convert decimal to binary in Java

---
+++ Dec2Bin.java
@@ -0,0 +1,20 @@
+// Converting a decimal number into binary in Java.
+// By Soham Gupta (http://github.com/gupta-soham/) (https://www.linkedin.com/in/soham-gupta-in/)
+public class Dec2Bin {
+    public static void decToBin(int n){
+        int Num = n;
+        int pow = 0;
+        int biNum = 0;
+        while(n > 0){
+            int rem = n % 2;
+            biNum = biNum + (rem * (int)Math.pow(10, pow)); 
+            pow++;
+            n /= 2;
+        }
+        System.out.println("Binary form of " + Num + " = " + biNum);
+    }    
+
+    public static void main(String[] args) {
+        decToBin(15);
+    }
+}
