9444f48e596687a1252d2fd3812dfd52866ca959
Anubhavroy12 <115141196+Anubhavroy12@users.noreply.github.com>
2022-10-12 23:26:38 +0530

    Update DiamondPattern.java
    please

--- Java Tutorial Program/DiamondPattern.java
+++ Java Tutorial Program/DiamondPattern.java
@@ -4,10 +4,10 @@ class DiamondPattern{
     int i,j,k;
     Scanner sc= new Scanner(System.in);
     System.out.print("Enter the number of row for diamond shape : ");  
-    int num = sc.nextInt();  
-    for(i=0;i<num;i++)
+    int n = sc.nextInt();  
+    for(i=0;i<n;i++)
     {
-	    for(j=0;j<num-i-1;j++){
+	    for(j=0;j<n-i-1;j++){
                      System.out.print(" ");
 	    }
 	    for(k=0;k<2*i+1;k++){
@@ -16,9 +16,9 @@ class DiamondPattern{
 		 System.out.println("");
 	    }
 	    
-	 for(i=num-1;i>0;i--)
+	 for(i=n-1;i>0;i--)
     {
-	    for(j=num-1;j>=i;j--){
+	    for(j=n-1;j>=i;j--){
                      System.out.print(" ");
 	    }
 	   for(k=2*i-1;k>0;k--){
