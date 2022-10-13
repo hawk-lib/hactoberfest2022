c78286bc5832dbd68e40928373b4f458af3711b6
Rahulrathour33 <90857830+Rahulrathour33@users.noreply.github.com>
2022-10-13 20:39:20 +0530

    Add files via upload

---
+++ c_programs/sum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+	int x;
+	int y;
+	printf("Enter The value of x :");
+	scanf("%d",&x);
+	printf("Enter the value of y :");
+	scanf("%d",&y);
+	printf("Sum of two number  %d:" ,x+y);
+	return 0;
+	
+}
