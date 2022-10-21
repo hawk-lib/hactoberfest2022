055ebd23f09ef0858c4b1e2aaf9df2f43d7354c5
Dipesh Das <77893779+Dipeshdas@users.noreply.github.com>
2022-10-21 15:03:45 +0530

    A new java file added by Dipesh

---
+++ java_programs/Array_RainWaterTrappingProblem.java
@@ -0,0 +1,28 @@
+package DSA;
+
+public class Array_RainWaterTrappingProblem {
+    public static int RTP(int a[]){
+        int n=a.length;
+        int left[]=new int[n];
+        int right[]=new int[n];
+        left[0]=a[0];
+        for(int i=1;i<n;i++){
+            left[i]=Math.max(left[i-1],a[i]);
+        }
+        right[n-1]=a[n-1];
+        for(int i=n-2;i>=0;i--){
+            right[i]=Math.max(right[i+1],a[i]);
+        }
+        int ans=0;
+        for (int i=0;i<a.length;i++){
+            ans+=Math.min(left[i],right[i])-a[i];
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int a[]={3,1,2,4,0,1,3,2};
+        System.out.println(RTP(a));
+
+    }
+}
